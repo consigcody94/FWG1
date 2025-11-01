07fd84709e5a5c89b016d3369f1eac79157e724e
Cody Churchwell <cody.churchwell@email.phoenix.edu>
2025-11-01 11:50:20 -0400

    Update netlify-identity.tsx

--- app/components/netlify-identity.tsx
+++ app/components/netlify-identity.tsx
@@ -4,41 +4,5 @@ import Script from "next/script";
 import { useEffect } from "react";
 
 export function NetlifyIdentity() {
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      // Handle the redirect from email confirmation
-      if (
-        window.location.hash &&
-        window.location.hash.includes("recovery_token")
-      ) {
-        // Redirect to admin with the token
-        window.location.href = `/admin/${window.location.hash}`;
-      } else if (
-        window.location.hash &&
-        window.location.hash.includes("confirmation_token")
-      ) {
-        // Redirect to admin with the token
-        window.location.href = `/admin/${window.location.hash}`;
-      } else if (
-        window.location.hash &&
-        window.location.hash.includes("invite_token")
-      ) {
-        // Redirect to admin with the token
-        window.location.href = `/admin/${window.location.hash}`;
-      }
-    }
-  }, []);
-
-  return (
-    <Script
-      src="https://identity.netlify.com/v1/netlify-identity-widget.js"
-      strategy="afterInteractive"
-      onLoad={() => {
-        const windowWithIdentity = window as any;
-        if (windowWithIdentity.netlifyIdentity) {
-          windowWithIdentity.netlifyIdentity.init();
-        }
-      }}
-    />
-  );
+  return null;
 }
