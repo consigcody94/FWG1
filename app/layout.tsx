a050f19b3058e0ca89a8402e4905b92a94f4da52
Cody Churchwell <cody.churchwell@email.phoenix.edu>
2025-11-01 12:11:46 -0400

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -130,6 +130,11 @@ export default function RootLayout({
           src="https://identity.netlify.com/v1/netlify-identity-widget.js"
           strategy="afterInteractive"
           onLoad={() => {
+            if (window.location.hash.includes("invite_token")) {
+              const token = window.location.hash.split("=")[1];
+              document.cookie = `nf_jwt=${token}`;
+              window.location.href = "/admin/";
+            }
             const windowWithIdentity = window as any;
             if (windowWithIdentity.netlifyIdentity) {
               windowWithIdentity.netlifyIdentity.on('init', (user: any) => {
