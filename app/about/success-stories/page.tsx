ca5c0fe80bb5f2bbc59854855664f7c670d16963
FWG Admin <cchurchwell@federalworking.com>
2025-11-19 14:24:41 -0500

    Fix: Pass required data prop to SuccessStoriesBlock component

--- app/about/success-stories/page.tsx
+++ app/about/success-stories/page.tsx
@@ -91,7 +91,7 @@ export default function SuccessStoriesPage() {
       </section>
 
       {/* Success Stories List - From Markdown CMS */}
-      <SuccessStoriesBlock />
+      <SuccessStoriesBlock data={{}} />
     </div>
   )
 }
